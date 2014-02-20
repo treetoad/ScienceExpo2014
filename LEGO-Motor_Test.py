e2a23c702cd2d36578f564d5491366a6242a6fe5
Cedar V <cedar@oobik.com>
2014-02-20 03:12:42 +0000

    now working with one motor

--- LEGO-Motor_Test.py
+++ LEGO-Motor_Test.py
@@ -13,22 +13,19 @@ from BrickPi import *   #import BrickPi.py file to use BrickPi operations
 BrickPiSetup()  # setup the serial port for communication
 
 BrickPi.MotorEnable[PORT_A] = 1 #Enable the Motor A
-BrickPi.MotorEnable[PORT_B] = 1 #Enable the Motor B
 
 BrickPiSetupSensors()   #Send the properties of sensors to BrickPi
 
 while True:
     print "Running Forward"
     BrickPi.MotorSpeed[PORT_A] = 200  #Set the speed of MotorA (-255 to 255)
-    BrickPi.MotorSpeed[PORT_B] = 200  #Set the speed of MotorB (-255 to 255)
     ot = time.time()
-    while(time.time() - ot < 3):    #running while loop for 3 seconds
+    while(time.time() - ot < 0.3):    #running while loop for 3 seconds
         BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
-        time.sleep(.1)              # sleep for 100 ms
+        time.sleep(.5)              # sleep for 500 ms
     print "Running Reverse"
     BrickPi.MotorSpeed[PORT_A] = -200  #Set the speed of MotorA (-255 to 255)
-    BrickPi.MotorSpeed[PORT_B] = -200  #Set the speed of MotorB (-255 to 255)
     ot = time.time()
-    while(time.time() - ot < 3):    #running while loop for 3 seconds
-        BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
-        time.sleep(.1)              # sleep for 100 ms
+    while(time.time() - ot < 0.3):    #running while loop for 3 seconds
+	 BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
+     	 time.sleep(.5)              # sleep for 500 ms
