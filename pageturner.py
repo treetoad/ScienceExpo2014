4c15869c5c0eff3780920c62a80bdae3a9f2aebd
Cedar V <cedar@oobik.com>
2014-02-28 14:47:45 +0000

    catching up on lots of work
    - mostly working now

--- pageturner.py
+++ pageturner.py
@@ -8,6 +8,9 @@
 # http://www.dexterindustries.com/
 # This code is for testing the BrickPi with a Lego Motor
 
+import time
+from subprocess import call
+import re
 from BrickPi import *   #import BrickPi.py file to use BrickPi operations
 
 
@@ -29,7 +32,7 @@ def move(motor, speed, duration):
     BrickPiUpdateValues()
     while(time.time() - ot < duration):
         BrickPiUpdateValues()
-        time.sleep(0.05)
+        time.sleep(0.01)
     BrickPi.MotorSpeed[motor] = 0
     BrickPiUpdateValues()
         
@@ -38,23 +41,28 @@ def stop(motor):
     BrickPi.MotorSpeed[motor] = 0
     BrickPiUpdateValues()
         
-
-def preturn_page():
-    print "preparing page to be flipped"
-
-def flip_page():    
-    print "flipping page"   
     
-def page_preturn():
-    move(wheel_motor, 65, 1)
+def halfturn_wheel():
+    move(wheel_motor, 80, 1.26) ## 0.63
 
 def reset_claw():
-    move(shooty_motor, -130, 1)
+##    move(shooty_motor, -130, 2)
+##    move(claw_motor, -30, 1.5)
+    ot = time.time()
+    BrickPi.MotorSpeed[shooty_motor] = -125
+    BrickPi.MotorSpeed[claw_motor] = -30
+    BrickPiUpdateValues()
+    while(time.time() - ot < 0.5):
+        BrickPiUpdateValues()
+        time.sleep(0.05)
+    BrickPi.MotorSpeed[shooty_motor] = 0
+    BrickPi.MotorSpeed[claw_motor] = 0
+    BrickPiUpdateValues()
     move(claw_motor, -30, 1.5)
 
 def position_flipper():
     print "get flipper right in front of page"
-    move(claw_motor, 18, 1.4)
+    move(claw_motor, 18, 1.4) #1.4 is sort of working
     
 def thrust_flipper():
     print "get flipper under page to turn"
@@ -71,6 +79,8 @@ def thrust_flipper():
 
 def flip_page():
     print "actually flip the page"
+    move(claw_motor, 120, 3)
+    
     
 def move_claw():
 
@@ -78,15 +88,33 @@ def move_claw():
     move(claw_motor, 90, 2)
     move(claw_motor, -45, 2)
     move(shooty_motor, -130, 1)
+
+def ocr():   
+    #Take an image from the RaspberryPi camera with sharpness 100(increases the readability of the text for OCR)
+    call ("raspistill -o j2.jpg -t 1 -sh 100", shell=True)
+    print "Image taken"
     
-initialize()
-reset_claw()
-page_preturn()
-position_flipper()
-thrust_flipper()
+    #Start the Tesseract OCR and save the text to out1.txt
+    call ("tesseract j2.jpg out1.txt", shell=True)
+    print "OCR complete"
+    
+    #Open the text file and split the paragraph to Sentences
+    fname="out1.txt"
+    f=open(fname)
+    content=f.read()
+    print content
+
+ocr()    
+#initialize()
+
+
+##reset_claw()
+##halfturn_wheel()
+##position_flipper()
+##thrust_flipper()
+##flip_page()
 
 ##5
-
 ##
 ##initialize()
 ##page_preturn()
